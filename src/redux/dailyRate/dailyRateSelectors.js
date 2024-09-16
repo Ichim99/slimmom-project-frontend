430952e7267a26f54ee670b10a413032bf9ead10
Ichim99 <140713412+Ichim99@users.noreply.github.com>
2024-09-16 07:20:25 +0000

    Deploying to gh-pages from @ Ichim99/slimmom-project-frontend@3eacbd37444d98f78c5c24a07d19c48504041707 🚀

--- src/redux/dailyRate/dailyRateSelectors.js
+++
@@ -1,9 +0,0 @@
-// Daily Rate
-export const getDailyRate = state => state.dailyRate.dailyRate;
-export const getDataUser = state => state.dailyRate.dataUser;
-export const getNotAllowedProducts = state =>
-  state.dailyRate.notAllowedProducts;
-export const getSummaries = state => state.dailyRate.summaries;
-export const getDate = state => state.dailyRate.currentDate;
-export const getIsLoading = state => state.dailyRate.loading;
-export const getError = state => state.dailyRate.error;
